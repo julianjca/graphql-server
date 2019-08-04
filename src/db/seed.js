1f7f24b742ab4d6b1b0316465c14a3a8cce673cd
julianjca <julianjca11@gmail.com>
2019-08-04 15:44:43 +0700

    adding db seeder

---
+++ src/db/seed.js
@@ -0,0 +1,19 @@
+const casual = require('casual');
+const _ = require('lodash');
+
+// create mock data with a seed, so we always get the same
+casual.seed(123);
+db.sync({ force: true }).then(() => {
+  _.times(10, () => {
+    return AuthorModel.create({
+      first_name: casual.first_name,
+      last_name: casual.last_name,
+    }).then((author) => {
+      return author.createBook({
+        title: casual.title,
+        cover_image_url: casual.url,
+        average_rating: casual.integer(-10, 10),
+      });
+    });
+  });
+});
