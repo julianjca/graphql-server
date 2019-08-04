faa83cf9da6568de6af986c2f6981da1e110548d
julianjca <julianjca11@gmail.com>
2019-08-04 15:35:41 +0700

    commenting db sync and fix error import module

--- server.js
+++ server.js
@@ -1,21 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 // Import Schema
-import { Author, Book } from './store';
-
-const books = [
-    { id: 1, title: 'The Trials of Brother Jero',  cover_image_url: 'ssdsds', average_rating: 8, authorId: 1 },
-    { id: 2, title: 'Half of a Yellow Sun',  cover_image_url: 'dsdsds', average_rating: 9, authorId: 3 },
-    { id: 3, title: 'Americanah',  cover_image_url: 'dsdsds', average_rating: 9, authorId: 3 },
-    { id: 4, title: 'King Baabu',  cover_image_url: 'sdsds', average_rating: 7, authorId: 1 },
-    { id: 5, title: 'Children of Blood and Bone',  cover_image_url: 'sdsds', average_rating: 7, authorId: 2 },
-  ];
-
-  const authors = [
-    { id: 1, first_name: 'Wole', last_name: 'Soyinka' },
-    { id: 2, first_name: 'Tomi', last_name: 'Adeyemi' },
-    { id: 3, first_name: 'Chimamanda', last_name: 'Adichie' },
-  ];
+const { Author, Book } = require('./src/store');
 
  const typeDefs = gql`
     type Author {
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const casual = require('casual');
-import _ from 'lodash';
+const _ = require('lodash');
 
 const db = new Sequelize('coolreads', null, null, {
   dialect: 'sqlite',
@@ -22,23 +22,23 @@ AuthorModel.hasMany(BookModel);
 BookModel.belongsTo(AuthorModel);
 
 // create mock data with a seed, so we always get the same
-casual.seed(123);
-db.sync({ force: true }).then(() => {
-  _.times(10, () => {
-    return AuthorModel.create({
-      first_name: casual.first_name,
-      last_name: casual.last_name,
-    }).then((author) => {
-      return author.createBook({
-        title: casual.title,
-        cover_image_url: casual.url,
-        average_rating: casual.integer(-10, 10),
-      });
-    });
-  });
-});
+// casual.seed(123);
+// db.sync({ force: true }).then(() => {
+//   _.times(10, () => {
+//     return AuthorModel.create({
+//       first_name: casual.first_name,
+//       last_name: casual.last_name,
+//     }).then((author) => {
+//       return author.createBook({
+//         title: casual.title,
+//         cover_image_url: casual.url,
+//         average_rating: casual.integer(-10, 10),
+//       });
+//     });
+//   });
+// });
 
 const Author = db.models.author;
 const Book = db.models.book;
 
-export { Author, Book };
+module.exports = { Author, Book };
